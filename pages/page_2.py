0a4edbe8284256d3bdfeef364eca7f9c9e5cdfb2
LeongWeiXin <wxleong.2020@scis.smu.edu.sg>
2023-01-17 17:18:05 +0800

    IVH-8 Modify CSS

--- pages/page_2.py
+++ pages/page_2.py
@@ -43,55 +43,59 @@ def view(fid, file, file_type):
             # st.write(dfs)
             st.image()
 
-# View All Files
-for display in display_list:
-    fid = display[0]
-    company = display[1]
-    file_name = display[2]
-    file_type = display[3]
-    col1, col2, col3 = st.columns([4,15,3])
-    with col1:
-        if file_type == "pdf":
-                image = Image.open(os.path.join("assets","pdf.png"))
-        if file_type == "png" or file_type == "jpeg" or file_type== "jpg":
-            image = Image.open(os.path.join("assets","picture.png"))
-        st.image(image)
-    with col2:
-        st.subheader(company)
-        st.write(file_name)
-    with col3: 
-        btn = st.button("View", key=str(fid))
+placeholder = st.empty()
+state = False
+with placeholder.container():
+    # View All Files
+    for display in display_list:
+        fid = display[0]
+        company = display[1]
+        file_name = display[2]
+        file_type = display[3]
+        col1, col2, col3 = st.columns([4,15,3])
+        with col1:
+            if file_type == "pdf":
+                    image = Image.open(os.path.join("assets","pdf.png"))
+            if file_type == "png" or file_type == "jpeg" or file_type== "jpg":
+                image = Image.open(os.path.join("assets","picture.png"))
+            st.image(image)
+        with col2:
+            st.subheader(company)
+            st.write(file_name)
+        with col3: 
+            btn = st.button("View", key=str(fid))
+        #If btn is pressed 
+        if btn:  
+            state = True
+            break
+if state:
+    #This would empty everything inside the container
+    placeholder.empty()
+    with placeholder.container():
+        view(fid, file, file_type)
 
 ############## CSS
 st.markdown("""
     <style>
 
-    section.main.css-k1vhr4.egzxvld3 > div > div:nth-child(1) > div > div
+    section.main.css-k1vhr4.egzxvld3 > div > div:nth-child(1) > div > div:nth-child(3) > div > div 
     {
         background-color: #F0F2F6;
     }
-    section.main.css-k1vhr4.egzxvld3 > div > div:nth-child(1) > div > div:nth-child(1) > div 
-    {
-        background-color: white;
-    }
-    section.main.css-k1vhr4.egzxvld3 > div > div:nth-child(1) > div > div:nth-child(2)
-    {
-        background-color: white;
-    }
-    section.main.css-k1vhr4.egzxvld3 > div > div:nth-child(1) > div > div > div.css-bzyszk.e1tzin5v2
+    
+    section.main.css-k1vhr4.egzxvld3 > div > div:nth-child(1) > div > div > div > div > div.css-bzyszk.e1tzin5v2
     {
         margin: auto;
         display: flex;
     }
-    section.main.css-k1vhr4.egzxvld3 > div > div:nth-child(1) > div > div > div.css-bzyszk.e1tzin5v2 > div:nth-child(1) > div > div > div > div
+    section.main.css-k1vhr4.egzxvld3 > div > div:nth-child(1) > div > div:nth-child(3) > div > div > div.css-bzyszk.e1tzin5v2 > div:nth-child(1) > div > div > div > div    
     {
         justify-content: center;
     }
-   section.main.css-k1vhr4.egzxvld3 > div > div:nth-child(1) > div > div > div.css-o7qwft.e1tzin5v2 > div:nth-child(1) > div > div:nth-child(2) > div > div > p 
-    {
+    section.main.css-k1vhr4.egzxvld3 > div > div:nth-child(1) > div > div:nth-child(3) > div > div > div.css-o7qwft.e1tzin5v2 > div:nth-child(1) > div > div:nth-child(2) > div > div > p    {
         padding-bottom: 10px;
     }
-    section.main.css-k1vhr4.egzxvld3 > div > div:nth-child(1) > div > div > div.css-yksnv9.e1tzin5v2 
+    section.main.css-k1vhr4.egzxvld3 > div > div:nth-child(1) > div > div:nth-child(3) > div > div > div.css-yksnv9.e1tzin5v2 
     {
         display: flex;
         margin: auto;
