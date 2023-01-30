538c8cb75a70bb23bdab076c658716eeb7aa5726
LeongWeiXin <wxleong.2020@scis.smu.edu.sg>
2023-01-30 16:44:04 +0800

    IVH-7 Partial Implementation

--- api/api.py
+++ api/api.py
@@ -70,6 +70,66 @@ def get_allFiles():
             if (len(results)==0):
                 response["status"] = "No files available"
     return response
+
+# INSERT EXTRACTED DATA
+@app.post("/insert_data")
+def insert_data():
+    data = request.get_json()
+    fid = data["fid"]
+    cid = data["cid"]
+    extracted_data = data["data"]
     
+    sql = (
+    "INSERT INTO extracted_data (fid, cid, data) VALUES (%s, %s, %s);"
+    )
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(sql, (fid, cid, extracted_data))
+    return {"message": "Added", "code": 201}, 201
+
+# RETRIEVE EXTRACTED DATA
+@app.post("/retrieve_data")
+def retrieve_data():
+    data = request.get_json()
+    cid = data["cid"]
+    print(cid)
+    sql = ("""SELECT * FROM extracted_data WHERE cid = %s;""")
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(sql, [cid])
+            results = cursor.fetchall()
+    return {"message": "Added", "code": 200, "data": results}, 200
+
+# Dummy Data
+@app.get("/get_symbols")
+def get_symbols():
+    symbols = {
+        "symbols": {
+        "AUD": "Australian",
+        "CAD": "Canada",
+        "CHF": "Swiss Franc",
+        "CNY": "Chinese Yuan",
+        "JPY": "Japanese Yen",
+        "USD": "United States"
+        }
+    }
+    return symbols
+
+@app.post("/get_currencies")
+def get_currencies():
+    data = request.get_json()
+    base = data["base"]
+    currencies = {
+        "rates": {
+            "AUD": 1.566015,
+            "CAD": 1.560132,
+            "CHF": 1.154727,
+            "CNY": 7.827874,
+            "JPY": 132.360679,
+            "USD": 1.23396,
+        }
+    }
+    return currencies
+
 if __name__ == '__main__':
     app.run(debug=True)
--- main.py
+++ main.py
@@ -6,14 +6,16 @@ import base64
 import time
 from st_clickable_images import clickable_images
 from st_pages import Page, show_pages, add_page_title
+import json
+import re
+from datetime import date
 from request import(
     get_all_companies,
-    add_file
+    get_currencies,
+    get_symbols,
+    retrieve_data,
+    insert_data
 )
-
-
-
-st.write("THIS IS Main PG")
 
 # Specify what pages should be shown in the sidebar, and what their titles and icons
 # should be
@@ -26,5 +28,219 @@ show_pages(
     ]
 )
 get_options = get_all_companies()["data"]
-st.write(get_options)
+if (len(get_options) == 0):
+    st.text("No company available")
+else:
+    options = list(range(len(get_options)))
+    option = st.selectbox(
+        'Company',
+        options, format_func=lambda x: get_options[x][1])
+        
+    # Get Selected Company ID    
+    selected_comID = get_options[option][0]
+    selected_comName = get_options[option][1]
 
+
+    # Retrieve data
+    get_data = retrieve_data(selected_comID)
+    print(type(get_data))
+
+    # Currency
+    symbols = get_symbols()
+
+    code = ["Remain Unchange"]
+    for key, value in symbols["symbols"].items():
+        code.append(key + "(" + value + ")")
+
+    print(code)
+    option = st.selectbox(
+        'Currency to convert',
+        code)
+       
+    if option!= "Remain Unchange":
+        symbol_to_covert = option[:option.find("(")]
+        st.write('You selected:', symbol_to_covert)
+
+        currencies = get_currencies(symbol_to_covert)
+        exchange_rate = currencies["rates"][symbol_to_covert]
+    else:
+        exchange_rate = 1
+    # Start/End Year
+    year = []
+    print(type(get_data["data"][0][3]))
+    for data in get_data["data"]:
+        result = json.loads(data[3])
+        base_currency = result["currency"]
+        print(base_currency)
+
+        for income_stat in result["income_statement"]:
+            if income_stat["year"] not in year:
+                year.append(income_stat["year"])
+        if result["balance_sheet"]["year"] not in year:
+                year.append(result["balance_sheet"]["year"])
+        if result["cash_flow"]["year"] not in year:
+                year.append(result["cash_flow"]["year"])
+        if result["other_metrics"]["year"] not in year:
+                year.append(result["other_metrics"]["year"])
+        year.sort()
+
+    start_year = st.selectbox(
+        'Start Year',
+        year)
+
+    if year.index(start_year) == len(year)-1:
+        endYear = []
+    else:
+        endYear = year[year.index(start_year)+1:]
+    end_year = st.selectbox(
+        'End Year',
+        endYear)
+    if endYear == []:
+        st.error("Must be 2 or more years", icon="🚨")
+        print(date.today().year)
+        print(type(date.today().year))
+    elif end_year == date.today().year:
+        st.error("End date cannot be this year", icon="🚨")
+        # Quarter
+    else:    
+        # Aggregrated value
+        income_statement = {
+            "Year": [],
+            "Revenue": [],
+            "Cost":[],
+            "GrossProfitLoss":[],
+            "NetProfitLoss":[]
+        }
+        balance_sheet = {
+            "year": [],
+            "totalEquities":[],
+            "totalLiabilities":[],
+            "totalAssets":[]
+        }
+        for data in get_data["data"]:
+            result = json.loads(data[3])
+            for income_stat in result["income_statement"]:
+                if income_stat["year"]>=start_year and income_stat["year"]<=end_year:
+                    grossPL = income_stat["grossProfit"] - income_stat["grossLoss"]
+                    netPL = income_stat["netProfit"] - income_stat["netLoss"]
+                    if str(income_stat["year"]) in income_statement["Year"]:
+                        position = income_statement["Year"].index(str(income_stat["year"]))
+                        income_statement["Revenue"][position] = (income_statement["Revenue"][position] + income_stat["revenue"]*exchange_rate)/2
+                        income_statement["Cost"][position] = (income_statement["Cost"][position] + income_stat["cost"]*exchange_rate)/2
+                        income_statement["GrossProfitLoss"][position] = (income_statement["GrossProfitLoss"][position] + grossPL*exchange_rate)/2
+                        income_statement["NetProfitLoss"][position] = (income_statement["NetProfitLoss"][position] + netPL*exchange_rate)/2
+
+                    else:
+                        income_statement["Year"].append(str(income_stat["year"]))
+                        income_statement["Revenue"].append(income_stat["revenue"]*exchange_rate)
+                        income_statement["Cost"].append(income_stat["revenue"]*exchange_rate)
+                        income_statement["GrossProfitLoss"].append(grossPL*exchange_rate)
+                        income_statement["NetProfitLoss"].append(netPL*exchange_rate)
+
+            
+            balance_sheet_result = result["balance_sheet"]
+                
+            if balance_sheet_result["year"]>=start_year and balance_sheet_result["year"]<=end_year:
+
+                if str(balance_sheet_result["year"]) in balance_sheet["year"]:
+                    position = balance_sheet["year"].index(str(balance_sheet_result["year"]))
+                    balance_sheet["totalEquities"][position] = (balance_sheet["totalEquities"][position] + balance_sheet_result["totalEquities"])/2
+                    balance_sheet["totalLiabilities"][position] = (balance_sheet["totalLiabilities"][position] + balance_sheet_result["totalLiabilities"])/2
+                    balance_sheet["totalAssets"][position] = (balance_sheet["totalAssets"][position] + balance_sheet_result["totalAssets"])/2
+                else:
+                    balance_sheet["year"].append(str(balance_sheet_result["year"]))
+                    balance_sheet["totalEquities"].append(balance_sheet_result["totalEquities"]*exchange_rate)
+                    balance_sheet["totalLiabilities"].append(balance_sheet_result["totalLiabilities"]*exchange_rate)
+                    balance_sheet["totalAssets"].append(balance_sheet_result["totalAssets"]*exchange_rate)
+
+
+    
+
+        # Create Dataframe
+        print("DF")
+        df_is = pd.DataFrame(data=income_statement)
+        df_is.rename({'GrossProfitLoss': 'Gross Profit/Loss', 'NetProfitLoss': 'Net Profit/Loss'}, axis=1, inplace=True)
+        print(df_is)
+            
+        df_bs = pd.DataFrame(data=balance_sheet)
+        df_bs.rename({'year': 'Year', 'totalEquities': 'Total Equities', 'totalLiabilities': 'Total Liabilities', 'totalAssets':'Total Assets'}, axis=1, inplace=True)
+
+        print(df_bs)
+
+
+############## CSS
+st.markdown("""
+    <style>
+
+    section.main.css-k1vhr4.egzxvld3 > div > div:nth-child(1) > div > div:nth-child(n+7) > div 
+    {
+        width: fit-content;
+        padding: 10px;
+        padding-left: 30px;
+        border: 1px solid black;
+        border-radius: 5px;
+    }
+    section.main.css-k1vhr4.egzxvld3 > div > div:nth-child(1) > div > div > div > div > div > canvas
+    {
+        width: fit-content;
+        padding: 10px;
+        border: 1px solid black;
+        border-radius: 5px;
+    }
+    section.main.css-k1vhr4.egzxvld3 > div > div:nth-child(1) > div > div:last-child > div,
+    section.main.css-k1vhr4.egzxvld3 > div > div:nth-child(1) > div > div:nth-child(9) > div,
+    section.main.css-k1vhr4.egzxvld3 > div > div:nth-child(1) > div > div:nth-child(10) > div
+    {
+        padding: 0px;
+        border: 0px;
+    }
+
+    </style>
+""", unsafe_allow_html=True)
+# TESTING
+# data = {
+#     "currency": "yen",
+#     "income_statement": [
+#         {
+#             "year": 2023,
+#             "quarter": None,
+#             "revenue": 2000,
+#             "cost": 30000,
+#             "grossProfit": 5000,
+#             "grossLoss": 900,
+#             "netProfit": 300,
+#             "netLoss": 200,
+#             "incomeTax": 8900,
+#             "interest": 800,
+#             "depreciation": 400
+#         }
+#     ],
+#     "balance_sheet": {
+#         "year": 2023,
+#         "totalEquitiesAndLiabilities": 100,
+#         "totalEquities": 1000,
+#         "totalLiabilities": 300,
+#         "totalAssets": 4000,
+#         "totalCurrentAssets": 3000,
+#         "totalNonCurrentAssets": 3000,
+#         "debt": 4000,
+#         "cash": 300
+#     },
+#     "cash_flow": {
+#         "year": 2023,
+#         "operatingNetCashFlow": 300,
+#         "investingNetCashFlow": 3000,
+#         "financingNetCashFlow": 3000
+#     },
+#     "other_metrics": {
+#         "year": 2023,
+#         "returnOnEquity": 3000,
+#         "returnOnAsset": 3000,
+#         "netInterestMargin": 430,
+#         "netInterestIncomeRatio": 3000,
+#         "costIncomeRatio": 5000,
+#         "ebidta": 3000
+#     }
+# }
+
+# print(insert_data(5, "AC-1904", data))
