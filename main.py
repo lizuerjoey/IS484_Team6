0ec17284c78eb8f1c1c563a592b66a66a48dc2d9
sknhzd <sakinahaz.2020@smu.edu.sg>
2023-02-01 18:07:54 +0800

    IVH-2 Partial Implementation of user's page input
    Getting the user's input on which page they want to extract the table's from

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ show_pages(
         Page("main.py", "Dashboard", "💹"),
         Page("pages/page_1.py", "Upload Reports", ":book:"),
         Page("pages/page_4.py", "Preview", ":book:"),
+        Page("pages/page_3.py", "Extract", ":page_facing_up:"),
         Page("pages/page_2.py", "Files", ":page_facing_up:")
     ]
 )
