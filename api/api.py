e6e329084f10b0078a2fc02fae824cc6d16f98e7
lizuerjoey <joey.lee.2020@scis.smu.edu.sg>
2023-03-11 09:43:29 +0800

    deployed api on pythonanywhere

--- api/api.py
+++ api/api.py
@@ -1,17 +1,24 @@
 import os
 import psycopg2
 from dotenv import load_dotenv
-from flask import Flask, jsonify, request
+from flask import Flask, request
 
-load_dotenv()
-app = Flask(__name__)
+api_folder = os.path.expanduser('~/mysite')
+load_dotenv(os.path.join(api_folder, '.env'))
+
 ######## DB CONNECTION ########
 url = os.environ.get("POSTGRESQL_CONNECTION_STRING")
-connection = psycopg2.connect(url)
 
-######## APIs ########
+# connection = psycopg2.connect(url)
+# print(connection)
+
+app = Flask(__name__)
+
+# ######## APIs ########
+
 @app.get("/get_companies")
 def get_companies():
+    connection = psycopg2.connect(url)
     with connection:
         with connection.cursor() as cursor:
             companies = (
@@ -25,11 +32,13 @@ def get_companies():
             }
             if (len(results)==0):
                 response["status"] = "No companies available"
-            
+
+    connection.close()
     return response
 
 @app.post("/add_company")
 def add_company():
+    connection = psycopg2.connect(url)
     data = request.get_json()
     cid = data["cid"]
     com_name = data["com_name"]
@@ -39,10 +48,13 @@ def add_company():
     with connection:
         with connection.cursor() as cursor:
             cursor.execute(sql, (cid, com_name))
+
+    connection.close()
     return {"message": "Added", "code": 201}, 201
 
 @app.post("/insert_file")
 def insert_file():
+    connection = psycopg2.connect(url)
     data = request.get_json()
     cid = data["cid"]
     file = data["file"]
@@ -53,10 +65,13 @@ def insert_file():
     with connection:
         with connection.cursor() as cursor:
             cursor.execute(sql, (cid, file, file_type))
+
+    connection.close()
     return {"message": "Added", "code": 201}, 201
 
 @app.get("/get_allFiles")
 def get_allFiles():
+    connection = psycopg2.connect(url)
     with connection:
         with connection.cursor() as cursor:
             sql = (
@@ -69,27 +84,33 @@ def get_allFiles():
             }
             if (len(results)==0):
                 response["status"] = "No files available"
+
+    connection.close()
     return response
 
 # INSERT EXTRACTED DATA
 @app.post("/insert_data")
 def insert_data():
+    connection = psycopg2.connect(url)
     data = request.get_json()
     fid = data["fid"]
     cid = data["cid"]
     extracted_data = data["data"]
-    
+
     sql = (
     "INSERT INTO extracted_data (fid, cid, data) VALUES (%s, %s, %s);"
     )
     with connection:
         with connection.cursor() as cursor:
             cursor.execute(sql, (fid, cid, extracted_data))
+
+    connection.close()
     return {"message": "Added", "code": 201}, 201
 
 # RETRIEVE EXTRACTED DATA
 @app.post("/retrieve_data")
 def retrieve_data():
+    connection = psycopg2.connect(url)
     data = request.get_json()
     cid = data["cid"]
     print(cid)
@@ -98,11 +119,14 @@ def retrieve_data():
         with connection.cursor() as cursor:
             cursor.execute(sql, [cid])
             results = cursor.fetchall()
+
+    connection.close()
     return {"message": "Added", "code": 200, "data": results}, 200
 
 # RETREIVE EVERYTHING IN DICTIONARY DB
 @app.get("/get_dict")
 def get_dict():
+    connection = psycopg2.connect(url)
     with connection:
         with connection.cursor() as cursor:
             dict = (
@@ -116,13 +140,14 @@ def get_dict():
             }
             if (len(results)==0):
                 response["status"] = "No data available"
-            
-    return response
 
+    connection.close()
+    return response
 
 # RETRIEVE SYNONYM PER TYPE AND WORD
 @app.post("/get_synonym")
 def get_synonym():
+    connection = psycopg2.connect(url)
     data = request.get_json()
     type = data["type"]
     word = data["word"]
@@ -134,22 +159,23 @@ def get_synonym():
     response = {"code": 200, "data": results}
     if (len(results)==0):
             response["status"] = "No data available"
-            
+
+    connection.close()
     return response, 200
 
-# INSERT SYNONYM 
+# INSERT SYNONYM
 @app.post("/add_synonym")
 def add_synonym():
+    connection = psycopg2.connect(url)
     data = request.get_json()
     did = data["did"]
-    synonym = data["synonym"]    
+    synonym = data["synonym"]
     sql = (
         "UPDATE dictionary SET synonym = %s WHERE did=%s;"
     )
     with connection:
         with connection.cursor() as cursor:
             cursor.execute(sql, (synonym, did))
-    return {"message": "Updated", "code": 200}, 200
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    connection.close()
+    return {"message": "Updated", "code": 200}, 200
