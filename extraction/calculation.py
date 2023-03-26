0a4b940dffde4f19cfaed50ab1604679794b3a08
lizuerjoey <joey.lee.2020@scis.smu.edu.sg>
2023-03-26 19:00:23 +0800

    IVH-21 added roa calculation

--- extraction/calculation.py
+++ extraction/calculation.py
@@ -19,28 +19,59 @@ def calculate_other_metrics(edited_dict, company_id):
         "netProfitMargin": 0,
         "ebidta": 0
     }
-
-    # (1) ROE (%) = (Net Profit / Total Equity)
-    # (2) ROA (%) = (Net Profit / Total Assets)
     
     date = ""
-    netProfitValue = 0
-    if len(edited_dict["income_statement"]) > 0:
-        for each_dict in edited_dict["income_statement"]:
+    fin_sheet = "income_statement"
+    other_fin_sheet = "balance_sheet"
+
+    returnOnEquity = 0
+    returnOnAsset = 0
+
+    # check if there are extracted data in income statement
+    # if none, check balance statement instead
+    if len(edited_dict[fin_sheet]) == 0:
+        fin_sheet = "balance_sheet"
+        other_fin_sheet = "income_statement"
+
+    if len(edited_dict[fin_sheet]) > 0:
+        for each_dict in edited_dict[fin_sheet]:
             for key, value in each_dict.items():
                 if key == "year":
                     date = value
                     other_metrics_format[key] = value
                 elif key == "numberFormat":
                     other_metrics_format[key] = value
-                elif key == "netProfit":
-                    netProfitValue = value
+                elif key == "netProfit" or key == "totalAssets" or key == "totalEquities":
+                    npv_te_ta = value
+
+                    if fin_sheet == "income_statement":
+
+                        if key == "totalEquities":
+                            # (1) ROE (%) = (Net Profit / Total Equity)
+                            returnOnEquity = cross_stmt_calculation(fin_sheet, date, "netProfit", npv_te_ta, company_id, "totalEquities", edited_dict[other_fin_sheet])
+                            st.write("ROE: " + str(returnOnEquity))
+
+                        if key == "totalAssets":
+                            # (2) ROA (%) = (Net Profit / Total Assets)
+                            returnOnAsset = cross_stmt_calculation(fin_sheet, date, "netProfit", npv_te_ta, company_id, "totalAssets", edited_dict[other_fin_sheet])
                     
-                    returnOnEquity = cross_stmt_calculation("income_statement", date, "netProfit", netProfitValue, company_id, "totalEquities", edited_dict["balance_sheet"])
-                    other_metrics_format["returnOnEquity"] = returnOnEquity    
-        
-                    # st.write("ROE: " + str(returnOnEquity))
-                    print(other_metrics_format)
+                    # balance sheet
+                    else:
+                        if key == "totalEquities":
+                            # (1) ROE (%) = (Net Profit / Total Equity)
+                            returnOnEquity = cross_stmt_calculation(fin_sheet, date, "totalEquities", npv_te_ta, company_id, "netProfit", edited_dict[other_fin_sheet])
+
+                        if key == "totalAssets":
+                            # (2) ROA (%) = (Net Profit / Total Assets)
+                            returnOnAsset = cross_stmt_calculation(fin_sheet, date, "totalAssets", npv_te_ta, company_id, "netProfit", edited_dict[other_fin_sheet])
+                    
+                    other_metrics_format["returnOnEquity"] = returnOnEquity
+                    other_metrics_format["returnOnAsset"] = returnOnAsset
+
+            print(other_metrics_format)
+
+    # both income and balance sheet are empty -> can't calculate any other metrics
+    return edited_dict
 
 # def ebitda()
 
@@ -58,11 +89,17 @@ def cross_stmt_calculation(curr_statement, year, curr_financial_words, curr_valu
 
     all_extracted_results  = retrieve_data(company_id)["data"]
     curr_statement_financial_value = float(curr_value)
-    curr_count = 0
+
+    # retrieved from current statement value hence identified 1 value 
+    if curr_statement_financial_value != 0:
+        curr_count = 1
+    else:
+        curr_count = 0
+    
     div_count = 0
     div_stmt_financial_value = 0
 
-    # check if current extracted balance sheet have value
+    # check if current extracted income statement/ balance sheet have value
     for each_dict in div_dict:
         if each_dict["year"] == str(year):
             div_stmt_financial_value += float(each_dict[div_financial_words])
@@ -76,11 +113,10 @@ def cross_stmt_calculation(curr_statement, year, curr_financial_words, curr_valu
         for curr_stmt in result[curr_statement]:
             if curr_stmt["year"] == str(year):
                 curr_statement_financial_value += float(curr_stmt[curr_financial_words])
-                print(curr_statement_financial_value)
                 if curr_stmt[curr_financial_words] != 0:
                     curr_count+=1
 
-        # check past statement
+        # check past statement(s)
         for stmt in result[div_stmt]:
             if stmt["year"] == str(year):
                 div_stmt_financial_value += float(stmt[div_financial_words])
@@ -91,9 +127,6 @@ def cross_stmt_calculation(curr_statement, year, curr_financial_words, curr_valu
     avg_div_value = 0
     if curr_count != 0:
         avg_curr_value = curr_statement_financial_value/curr_count
-        st.write("V: " + str(div_stmt_financial_value))
-        st.write("C: " + str(div_count))
-    
     if div_count != 0:
         avg_div_value = div_stmt_financial_value/div_count
 
@@ -105,8 +138,8 @@ def cross_stmt_calculation(curr_statement, year, curr_financial_words, curr_valu
             return 0
     else:
         # cannot be divided by 0
-        if avg_div_value != 0:
-            return avg_curr_value/avg_div_value
+        if avg_curr_value != 0:
+            return avg_div_value/avg_curr_value
         else:
             return 0
 
